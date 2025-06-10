041ba62b596d782b834cbe9c084d550b1fb58dce
ireusmarques <ireusmarques@gmail.com>
2025-06-10 19:43:27 +0000

    movimentar a peças do xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+//Define o tabuleiro
+char tabuleiro[8][8];
+//Função para inicializar o tabuleiro
+void inicializaTabuleiro() {
+    for (int i = 0; i <8; i++) {
+        for (int j = 0; j < 8; j++) {
+            tabuleiro[i][j] = '.';
+
+        }
+
+    }
+
+}
+
+//Função para imprimir o tabuleiro
+void imprimeTabuleiro() {
+    printf(" a b c d e f g h\n");
+    for (int i = 0; i <8; i++) {
+        printf("%d", i+1);
+        for (int j = 0; j <8; j++) {
+            printf("%c", tabuleiro[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+}
+
+// Função para movimentar a Torre 
+void movimentaTorre(int x1, int y1, int x2, int y2) {
+    if (x1 == x2|| y1 == y2) {
+        tabuleiro[x2][y2]= 'T';
+        tabuleiro[x1][y1]= '.';
+        printf("Torre movida de (%d, %d)para (%d, %d)\n", x1 + 1, y1 + 'a', x2 + 1, y2 + 'a');
+    } else {
+
+        printf("Movimento inválido para a torre\n");
+
+    }
+
+}
+
+// Função para movimentar o bispo
+void movimentaBispo(int x1, int y1, int x2, int y2) {
+    if (abs(x2 - x1) == abs(y2 - y1)) {
+        tabuleiro[x2][y2] = 'B';
+        tabuleiro[x1][y1] = '.';
+        printf("Bispo movido de (%d, %d)para(%d, %d)\n", x1 + 1, y1 + 'a', x2 + 1, y2 + 'a');
+    } else { 
+        printf("Movimento inválido para o Bispo\n");
+
+    }
+
+}
+
+// Função para movimentar a Rainha
+void movimentaRainha(int x1, int y1, int x2, int y2) {
+    if (x1 == x2 || y1 ==y2 || abs(x2 - x1) == abs(y2 - y1)) {
+        tabuleiro[x2][y2] = 'R';
+        tabuleiro[x1][y1] = '.';
+        printf("Rainha movida de (%d, %d)para (%d, %d)\n", x1 + 1, y1 + 'a', x2 +1, y2 + 'a');
+    } else { 
+        printf("Movimento inválido para a Rainha\n");
+    }
+
+}
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    inicializaTabuleiro();
+    tabuleiro[0][0] = 'T'; //Torre
+    tabuleiro[0][2] = 'B', //bISPO
+    tabuleiro[0][3] = 'R'; //Rainha
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    imprimeTabuleiro();
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    movimentaTorre(0,0,0,4);
+    movimentaBispo(0,2,4,6);
+    movimentaRainha(0,3,5,3);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    imprimeTabuleiro();
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
